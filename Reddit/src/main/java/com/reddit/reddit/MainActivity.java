5c009cb5e5fa0290d9b20c4cf24ecf0cf351376e
Hung Truong <hung@hung-truong.com>
2014-02-26 20:42:29 -0500

    internet stuff

--- Reddit/src/main/java/com/reddit/reddit/MainActivity.java
+++ Reddit/src/main/java/com/reddit/reddit/MainActivity.java
@@ -1,17 +1,49 @@
 package com.reddit.reddit;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.apache.http.params.HttpProtocolParams;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends ListActivity {
+    private JSONArray stories;
+    private ListView mListView;
+    private ListAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        //setContentView(R.layout.activity_main);
+        new JSONAsyncTask().execute("http://www.reddit.com/.json");
+        mAdapter = new ListAdapter();
+        this.setListAdapter(mAdapter);
+}
 
 
     @Override
@@ -32,6 +64,146 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    private class JSONAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+            try {
+                JSONObject obj = new JSONObject(result);
+                stories = ((JSONObject)obj.get("data")).getJSONArray("children");
+                mAdapter.notifyDataSetChanged();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    private class ListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            if (stories != null)
+            {
+                 return stories.length();
+            }
+            else {
+                return 0;
+            }
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+            View v = inflater.inflate(R.layout.list_cell, parent, false);
+            JSONObject story;
+            try {
+                story = stories.getJSONObject(position);
+                story = story.getJSONObject("data");
+                ImageView imageView = (ImageView)v.findViewById(R.id.imageView);
+                TextView textView = (TextView)v.findViewById(R.id.textView);
+                textView.setText(story.getString("title"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            return v;
+        }
+    }
+
+    private class ListCellAdapter extends CursorAdapter{
+
+        private Context mContext;
+        private LayoutInflater mInflater;
+        private Cursor mCursor;
+
+        public ListCellAdapter(Context context, Cursor c, boolean autoRequery) {
+            super(context, c, autoRequery);
+            mContext = context;
+            mInflater = LayoutInflater.from(context);
+            mCursor = c;
+
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View v = inflater.inflate(R.layout.list_cell, parent, false);
+            bindView(v, context, cursor);
+            return v;
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
+            TextView textView = (TextView)view.findViewById(R.id.textView);
+            textView.setText("YOLO");
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 
 }
